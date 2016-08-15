4a9ea18fb51102b667168c6f5e8d95ae1ffbb18e
Your Name <you@example.com>
2016-08-15 14:20:32 -0400

    map over list

--- src/cljs/todolist/core.cljs
+++ src/cljs/todolist/core.cljs
@@ -7,9 +7,34 @@
 ;; -------------------------
 ;; Views
 
+(def todolist (atom [{:id 10:name "pick up the groceries"}
+                     {:id 9999 :name "wash the car"}
+                     {:id 888 :name "do the laundry"}]))
+(def newname (atom ""))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Hell"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [:div [:h2 "Welcome to todolist"]
+   (map-indexed (fn [idx item]
+                  [:div "MY TODO NAME IS: "
+                   (:name item)
+                   " and my index is " idx
+                   " and my id is " (:id item)
+                   ]
+                  )
+                @todolist)
+   [:input {:on-change (fn [event]
+                         (let [_val (.-value
+                                     (.-currentTarget
+                                      event))]
+                           (reset! newname _val )))}]
+
+   [:span {:id "test"
+           :className "testClassName"
+           :on-click (fn [event]
+                       (swap! todolist (fn [_todolist]
+                                         (conj _todolist {:id 2000
+                                                          :name @newname}))))}
+    "add item"]])
 
 (defn about-page []
   [:div [:h2 "About todolist"]
